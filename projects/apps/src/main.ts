6b0bffdb25fae959d8570208ee7b7c468b432f4e
Alberto Basalo <albertobasalo@hotmail.com>
2022-11-18 11:55:45 +0100

    feat: bootstrap application

--- projects/apps/src/main.ts
+++ projects/apps/src/main.ts
@@ -1,6 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [],
-});
+bootstrapApplication(AppComponent);
